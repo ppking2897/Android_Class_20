9c979c4ab924b98ee0551109c817c6397d8385e5
ppking <ppking2897@gmail.com>
2016-11-29 17:05:07 +0800

    讀取手機機碼、sin卡碼
    聯絡人 在LOG裡
    
    讀取最後一張照片在imgView

--- app/src/main/java/com/example/user/android_class_20/MainActivity.java
+++ app/src/main/java/com/example/user/android_class_20/MainActivity.java
@@ -3,7 +3,13 @@ package com.example.user.android_class_20;
 import android.Manifest;
 import android.accounts.Account;
 import android.accounts.AccountManager;
+import android.content.ContentResolver;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.provider.ContactsContract;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -12,22 +18,27 @@ import android.os.Bundle;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
 import android.util.Log;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     private TelephonyManager tmgr;
     private AccountManager amgr;
-
+    private ImageView img;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        img = (ImageView)findViewById(R.id.img);
+
         if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.GET_ACCOUNTS)
+                Manifest.permission.READ_EXTERNAL_STORAGE)
                 != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this,
                     new String[]{Manifest.permission.READ_PHONE_STATE,
-                            Manifest.permission.GET_ACCOUNTS},
+                            Manifest.permission.GET_ACCOUNTS,
+                            Manifest.permission.READ_CONTACTS,
+                            Manifest.permission.READ_EXTERNAL_STORAGE},
                     123);
         }else{
             init();
@@ -46,9 +57,45 @@ public class MainActivity extends AppCompatActivity {
         for (Account a: as){
             Log.v("brad", a.name + ":" +a.type );
         }
-
+        //getContact();
+        getphoto();
 
     }
+
+
+    private void getContact(){
+        Log.v("ppking", "OK");
+        ContentResolver contentResolver = getContentResolver();
+        String[] projection = {ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+                ContactsContract.CommonDataKinds.Phone.NUMBER
+        };
+        Cursor cursor = contentResolver.query(
+                ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                projection, null, null,
+                ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME);
+
+        Log.v("ppking", "count: " + cursor.getCount());
+
+        while ( cursor.moveToNext()){
+            String name = cursor.getString(0);
+            String tel = cursor.getString(1);
+            Log.v("ppking", name + ":" + tel);
+        }
+    }
+
+    private void getphoto(){
+        ContentResolver contentResolver = getContentResolver();
+        Cursor c = contentResolver.query( MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                                            null, null, null, null);
+        Log.v("ppking", "photo: " + c.getCount());
+        c.moveToLast();
+        String data = c.getString(c.getColumnIndex(MediaStore.Images.Media.DATA));
+
+        Bitmap bp = BitmapFactory.decodeFile(data);
+        img.setImageBitmap(bp);
+
+    }
+
     private class MyPhoneStateListener extends PhoneStateListener {
         @Override
         public void onCallStateChanged(int state, String incomingNumber) {
